aef7e87fc0f99dc5666fcc1302481002722d1993
Rafael Oliveira <ludug3r0@gmail.com>
2015-02-24 23:17:04 -0300

    Complete TodoMVC seems to work fine

--- src/cljs/reagent_tech_radar/core.cljs
+++ src/cljs/reagent_tech_radar/core.cljs
@@ -1,49 +1,7 @@
 (ns reagent-tech-radar.core
     (:require [reagent.core :as reagent :refer [atom]]
-              [reagent.session :as session]
-              [secretary.core :as secretary :include-macros true]
-              [goog.events :as events]
-              [goog.history.EventType :as EventType]
-              [cljsjs.react :as react])
+              [reagent-tech-radar.todo :as todo])
     (:import goog.History))
 
-;; -------------------------
-;; Views
-
-(defn home-page []
-  [:div [:h2 "Welcome to reagent-tech-radar"]
-   [:div [:a {:href "#/about"} "go to about page"]]])
-
-(defn about-page []
-  [:div [:h2 "About reagent-tech-radar"]
-   [:div [:a {:href "#/"} "go to the home page"]]])
-
-(defn current-page []
-  [:div [(session/get :current-page)]])
-
-;; -------------------------
-;; Routes
-(secretary/set-config! :prefix "#")
-
-(secretary/defroute "/" []
-  (session/put! :current-page #'home-page))
-
-(secretary/defroute "/about" []
-  (session/put! :current-page #'about-page))
-
-;; -------------------------
-;; History
-;; must be called after routes have been defined
-(defn hook-browser-navigation! []
-  (doto (History.)
-    (events/listen
-     EventType/NAVIGATE
-     (fn [event]
-       (secretary/dispatch! (.-token event))))
-    (.setEnabled true)))
-
-;; -------------------------
-;; Initialize app
 (defn init! []
-  (hook-browser-navigation!)
-  (reagent/render-component [current-page] (.getElementById js/document "app")))
+  (reagent/render-component [todo/todo-app] (.getElementById js/document "app")))
